79b8dbbef0727825bc4d9b8657bae6f0c9b14033
Andrew <kellyaj@gmail.com>
2012-12-03 00:33:10 -0600

    Validation of event_date not being in the past

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,16 @@ class Event < ActiveRecord::Base
   attr_accessible :event_date, :location, :group_id
 
   has_many :game_suggestions
- 	has_many :rsvps
+  has_many :rsvps
 
- #Callback to enforce date is not in the past
+  validates_presence_of :location, :event_date
+
+  validate :not_past_date
+
+  def not_past_date
+    if self.event_date < Date.today 
+      errors.add(:event_date,'is in the past. Got a time machine?')
+    end
+  end
+
 end
